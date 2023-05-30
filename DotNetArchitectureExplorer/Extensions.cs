b29b8f1ae30053fd72853b930c98856dfea65f5a
beyaz <beyaz1404@gmail.com>
2023-05-30 14:36:05 +0300

    .

---
+++ DotNetArchitectureExplorer/Extensions.cs
@@ -0,0 +1,61 @@
+﻿using Mono.Cecil;
+
+namespace DotNetArchitectureExplorer;
+
+static class Extensions
+{
+
+    /// <summary>
+    ///     Removes value from start of str
+    /// </summary>
+    public static string RemoveFromStart(this string data, string value)
+    {
+        return RemoveFromStart(data, value, StringComparison.OrdinalIgnoreCase);
+    }
+
+    /// <summary>
+    ///     Removes value from start of str
+    /// </summary>
+    public static string RemoveFromStart(this string data, string value, StringComparison comparison)
+    {
+        if (data == null)
+        {
+            return null;
+        }
+
+        if (data.StartsWith(value, comparison))
+        {
+            return data.Substring(value.Length, data.Length - value.Length);
+        }
+
+        return data;
+    }
+
+    public static void ForEachType(this AssemblyDefinition assemblyDefinition, Action<TypeDefinition> action)
+    {
+        foreach (var moduleDefinition in assemblyDefinition.Modules)
+        {
+            foreach (var type in moduleDefinition.Types)
+            {
+                action(type);
+            }
+        }
+    }
+
+
+    public static TypeDefinition FindType(this AssemblyDefinition assemblyDefinition, Func<TypeDefinition, bool> action)
+    {
+        foreach (var moduleDefinition in assemblyDefinition.Modules)
+        {
+            foreach (var type in moduleDefinition.Types)
+            {
+                if (action(type))
+                {
+                    return type;
+                }
+            }
+        }
+
+        return null;
+    }
+}
