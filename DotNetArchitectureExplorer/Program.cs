5cf1da727bfd24a68fcdce1e50a8f343fc234a39
beyaz <beyaz1404@gmail.com>
2023-05-30 18:49:01 +0300

    .

--- DotNetArchitectureExplorer/Program.cs
+++ DotNetArchitectureExplorer/Program.cs
@@ -1,7 +1,11 @@
 ﻿
-var assemblyFilePath = @"C:\github\DotNetArchitectureExplorer\DotNetArchitectureExplorer\bin\Debug\net6.0\DotNetArchitectureExplorer.dll";
+//var assemblyFilePath = @"C:\github\DotNetArchitectureExplorer\DotNetArchitectureExplorer\bin\Debug\net6.0\DotNetArchitectureExplorer.dll";
 
-var fullTypeName = "DotNetArchitectureExplorer.Extensions";
+//var fullTypeName = "DotNetArchitectureExplorer.Extensions";
+
+var assemblyFilePath = @"C:\github\ReactWithDotNet\ReactWithDotNet\bin\Debug\net6.0\ReactWithDotNet.dll";
+
+var fullTypeName = "ReactWithDotNet.ElementSerializer";
 
 var (exception, dgmlContent) = CreateMethodCallGraph(assemblyFilePath, fullTypeName);
 if (exception is null)
