29c30bdea84a86149f2efcd262432506902dd067
beyaz <beyaz1404@gmail.com>
2023-05-30 17:46:12 +0300

    .

--- DotNetArchitectureExplorer/Program.cs
+++ DotNetArchitectureExplorer/Program.cs
@@ -3,4 +3,8 @@
 
 using DotNetArchitectureExplorer;
 
-Handler.CreateMethodCallGraph(@"C:\github\DotNetArchitectureExplorer\DotNetArchitectureExplorer\bin\Debug\net6.0\DotNetArchitectureExplorer.dll", "DotNetArchitectureExplorer.Extensions");
+var assemblyFilePath = @"C:\github\DotNetArchitectureExplorer\DotNetArchitectureExplorer\bin\Debug\net6.0\DotNetArchitectureExplorer.dll";
+
+var fullTypeName = "DotNetArchitectureExplorer.Extensions";
+
+Handler.CreateMethodCallGraph(assemblyFilePath, fullTypeName);
