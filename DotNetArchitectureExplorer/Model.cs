c3ecd679f1bfa76fcdb62b2459cb950ce42a470b
beyaz <beyaz1404@gmail.com>
2023-06-01 15:33:28 +0300

    .

--- DotNetArchitectureExplorer/Model.cs
+++ DotNetArchitectureExplorer/Model.cs
@@ -4,16 +4,7 @@ namespace DotNetArchitectureExplorer;
 
 static class Extensions2
 {
-    public static IEnumerable<Node> ConnectedNodes(this BinaryDecisionTree bdt)
-    {
-        return bdt.Links.SelectMany(v => new[]
-            {
-                v.Source,
-                v.Target
-            })
-            .Distinct()
-            .ToList();
-    }
+   
 }
 
 public class BinaryDecisionTree
@@ -48,23 +39,38 @@ public static class DgmlHelper
     public static XElement ToDgml(this BinaryDecisionTree bdt)
     {
         var nodes =
-            from n in bdt.ConnectedNodes()
+            from n in ConnectedNodes(bdt)
             select n.ToDgml();
+        
         var links =
             from v in bdt.Links
             select v.ToDgml();
+        
         return CreateGraph(nodes, links);
+
+        static XElement CreateGraph(IEnumerable<XElement> nodes, IEnumerable<XElement> links)
+        {
+            var xElement = new XElement(XName.Get("DirectedGraph", ns));
+            var xElement2 = new XElement(XName.Get("Nodes", ns));
+            var xElement3 = new XElement(XName.Get("Links", ns));
+            xElement2.Add(nodes.Cast<object>().ToArray());
+            xElement3.Add(links.Cast<object>().ToArray());
+            xElement.Add(xElement2);
+            xElement.Add(xElement3);
+            return xElement;
+        }
+
+        static IEnumerable<Node> ConnectedNodes(BinaryDecisionTree bdt)
+        {
+            return bdt.Links.SelectMany(v => new[]
+                {
+                    v.Source,
+                    v.Target
+                })
+                .Distinct()
+                .ToList();
+        }
     }
 
-    static XElement CreateGraph(IEnumerable<XElement> nodes, IEnumerable<XElement> links)
-    {
-        var xElement = new XElement(XName.Get("DirectedGraph", ns));
-        var xElement2 = new XElement(XName.Get("Nodes", ns));
-        var xElement3 = new XElement(XName.Get("Links", ns));
-        xElement2.Add(nodes.Cast<object>().ToArray());
-        xElement3.Add(links.Cast<object>().ToArray());
-        xElement.Add(xElement2);
-        xElement.Add(xElement3);
-        return xElement;
-    }
+   
 }
