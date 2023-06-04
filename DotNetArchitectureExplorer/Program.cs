3db2c75eaa131f6e745ab5357327152106aab7f7
beyaz <beyaz1404@gmail.com>
2023-06-04 19:45:34 +0300

    .

--- DotNetArchitectureExplorer/Program.cs
+++ DotNetArchitectureExplorer/Program.cs
@@ -1,7 +1,8 @@
 ﻿
 //var assemblyFilePath = @"C:\github\DotNetArchitectureExplorer\DotNetArchitectureExplorer\bin\Debug\net6.0\DotNetArchitectureExplorer.dll";
-var assemblyFilePath = @"C:\github\ReactWithDotNet\ReactWithDotNet\bin\Debug\net6.0\ReactWithDotNet.dll";
+//var assemblyFilePath = @"C:\github\ReactWithDotNet\ReactWithDotNet\bin\Debug\net6.0\ReactWithDotNet.dll";
 
+var assemblyFilePath = @"C:\github\DotNetArchitectureExplorer\DotNetArchitectureExplorer\bin\Debug\net6.0\Mono.Cecil.dll";
 var (exception, dgmlContent) = CreateMethodCallGraphOfAssembly(assemblyFilePath);
 if (exception is null)
 {
